cdf63fbf923b5653fbd2b7047cd55b545db693b9
Bob Groeneveld <bob.groeneveld@gmail.com>
2013-07-30 18:09:22 +0200

    fixed typo in sh script

--- start-chrome-for-playr.sh
+++ start-chrome-for-playr.sh
@@ -14,7 +14,7 @@ fi
 if [ $2 -eq "" ]
 then
 	# enter the location of the plpayr-loader.html file
-	reload_url=file://${playr_loader_file"
+	reload_url=file://${playr_loader_file
 else
 	reload_url=$2
 fi
