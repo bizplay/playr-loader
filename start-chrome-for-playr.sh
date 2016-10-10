ac767387e58d89824485545a4de82ac1b96bde6d
Bob Groeneveld <bob.groeneveld@gmail.com>
2016-10-10 23:41:47 +0200

    made both shell scripts (linux and osx) more uniform

--- start-chrome-for-playr.sh
+++ start-chrome-for-playr.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+
+# This batch file is provided to show digital signage content from playr.biz
+# To read more on the purpose of this file and how to use it
+# see the accompanying README.md file or
+# contact your digital signage provider.
+#
+# This file is licensed under the MIT license.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 # The path to the page that will check internet connection 
 # before loading the playr channel
 playr_loader_file="/home/webc/playr-loader/playr_loader.html"
 
 if [[ $1 == "" ]]
 then
-	# enter the channel url below so it is equal to the 
+	# use the generic URL below for ease of use
+	# or use the channel url that is shown as
 	# Playback Address on your dashboard
 	channel="http://play.playr.biz"
 else
@@ -20,4 +38,6 @@ else
 	reload_url=file://$2
 fi
 
+# to check the values of the variables created above uncomment the following line
+# echo "file://"${playr_loader_file}"?channel="${channel}"&reload_url="${reload_url}
 google-chrome --no-first-run --no-default-browser-check --disable-translate --disable-session-crashed-bubble --kiosk "file://"${playr_loader_file}"?channel="${channel}"&reload_url="${reload_url}
--- start-chrome-osx.sh
+++ start-chrome-osx.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+
+# This batch file is provided to show digital signage content from playr.biz
+# To read more on the purpose of this file and how to use it
+# see the accompanying README.md file or
+# contact your digital signage provider.
+#
+# This file is licensed under the MIT license.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 # The path to the page that will check internet connection 
 # before loading the playr channel
 playr_loader_file="/Users/user-name/playr_loader/playr_loader.html"
 
 if [[ $1 == "" ]]
 then
-	# enter the channel url below so it is equal to the 
+	# use the generic URL below for ease of use
+	# or use the channel url that is shown as
 	# Playback Address on your dashboard
-	channel="http://playr.biz/xxxx/yyyy"
+	channel="http://play.playr.biz"
 else
 	channel=$1
 fi
@@ -20,5 +38,6 @@ else
 	reload_url=file://$2
 fi
 
+# to check the values of the variables created above uncomment the following line
 # echo "file://"${playr_loader_file}"?channel="${channel}"&reload_url="${reload_url}
 open -a "/Applications/Google Chrome.app" --args --no-first-run --no-default-browser-check --disable-translate --disable-session-crashed-bubble --kiosk "file://"${playr_loader_file}"?channel="${channel}"&reload_url="${reload_url}
